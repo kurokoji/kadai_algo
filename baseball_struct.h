611b2d835dbf0e1c96e3ddd066ce8984ddf8517a
kurokoji <shu.kakihana@gmail.com>
2018-01-31 14:16:31 +0900

    [update] handの配列 数を拡張した

--- baseball_struct.h
+++ baseball_struct.h
@@ -9,7 +9,7 @@ typedef struct Player {
   int age; // 年齢
   int height; // 身長
   int weight; // 体重
-  char hand[4]; // 投打の利き手
+  char hand[16]; // 投打の利き手
   char school_name[64]; // 出身校
   char team_name[64]; // 所属チーム
 } Player;
